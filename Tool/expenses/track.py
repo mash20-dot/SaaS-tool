e56b445f104eb425e8eea48a95a0f30a17cf4a98
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-10-18 16:41:26 +0000

    hd

--- Tool/expenses/track.py
+++ Tool/expenses/track.py
@@ -63,13 +63,6 @@ def get_expenses():
     return jsonify(result), 200
 
 
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from sqlalchemy import extract, func, desc
-from datetime import datetime
-
-# assuming your blueprint
-# expenses = Blueprint('expenses', __name__)
 
 @expenses.route('/track/summary', methods=['GET'])
 @jwt_required()
