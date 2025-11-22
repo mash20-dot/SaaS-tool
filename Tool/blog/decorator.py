fcf811852c681b1f7c92d67212c2c13ff6758706
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-11-22 20:32:24 +0000

    allowed cors

--- Tool/blog/decorator.py
+++ Tool/blog/decorator.py
@@ -1,6 +1,6 @@
 from functools import wraps
-from flask import jsonify
-from flask_jwt_extended import get_jwt_identity
+from flask import jsonify, request
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
 from app.models import User
 
 def role_required(*allowed_roles):
@@ -10,6 +10,13 @@ def role_required(*allowed_roles):
         @wraps(fn)
         #accepting any kind of input in the decorated function
         def decorated(*args, **kwargs):
+
+            # Allow OPTIONS (CORS preflight)
+            if request.method == "OPTIONS":
+                return fn(*args, **kwargs)
+
+            # Verify JWT first
+            verify_jwt_in_request()
             current_email = get_jwt_identity()
 
             current_user = User.query.filter_by(email=current_email).first()
